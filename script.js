d2e4ba090c2c92084459c9ac45c04757c95f8e6f
Saksham Verma <sakshamverma1000@gmail.com>
2024-11-19 09:21:40 +0530

    Started Page 3 Development

--- script.js
+++ script.js
@@ -36,22 +36,30 @@ const page2BottomAnimation = () => {
     scrollTrigger: {
       trigger: "#emptyDiv",
       scroller: "body",
-      start: "top 50%",
-      end: "bottom 29%",
-      markers: true,
-      scrub: 3,
+      start: "top 75%",
+      end: "top 35%",
+      markers: false,
+      scrub: 5,
     },
   });
 
   elem3Text.forEach((heading, index) => {
-    t2.from(heading, {
-      y: 100,
-      duration: 3,
-      zIndex: 14,
-      delay: 2,
-      // opacity: 0,
-      ease: "back",
-    });
+    t2.fromTo(
+      heading,
+      {
+        y: 100,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1.2,
+        delay: 0.5,
+        stagger: 0.5,
+        // opacity: 0,
+        ease: "back",
+      }
+    );
   });
 };
 
@@ -81,42 +89,9 @@ const cursorEffect = () => {
 
 cursorEffect();
 
-/************* LOCOMOTIVE SCROLL TRIGGER CODEPEN CODE */
+/************* Lennis CODE */
 /************************************************************** */
-
-/*Using Locomotive Scroll from Locomotive https:github.com/locomotivemtl/locomotive-scroll
-
- const locoScroll = new LocomotiveScroll({
-   el: document.querySelector("#main"),
-   smooth: true,
- });
-  each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
- locoScroll.on("scroll", ScrollTrigger.update);
-
-  tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
- ScrollTrigger.scrollerProxy("#main", {
-   scrollTop(value) {
-     return arguments.length
-       ? locoScroll.scrollTo(value, 0, 0)
-       : locoScroll.scroll.instance.scroll.y;
-   },  we don't have to define a scrollLeft because we're only scrolling vertically.
-   getBoundingClientRect() {
-     return {
-       top: 0,
-       left: 0,
-       width: window.innerWidth,
-       height: window.innerHeight,
-     };
-   },
-    LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-   pinType: document.querySelector("#main").style.transform
-     ? "transform"
-     : "fixed",
- });
-
-  each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
- ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-  after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
- ScrollTrigger.refresh();
- */
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
